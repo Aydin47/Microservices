11bc7ef755ea57a814c3e70e600ce004a0389e34
Aydin Zahedian <aydin.zahedian@gmail.com>
2020-10-27 20:35:56 +0100

    Deleted unneeded files

--- Database1/Table1.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table1]
-(
-	[id] INT NOT NULL PRIMARY KEY, 
-    [Date] DATETIME NOT NULL, 
-    [TemperatureC] INT NOT NULL, 
-    [TemperatureF] INT NOT NULL, 
-    [Summary] NCHAR(10) NOT NULL 
-)
